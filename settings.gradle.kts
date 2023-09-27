47cb34e580355254adb4d5dd0cd6d32b9ed94998
klym4uk <klym4uk.ant0n@gmail.com>
2023-09-27 13:08:17 +0300

    Innitional commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Savchenko LAB 1"
+include(":app")
+ 
